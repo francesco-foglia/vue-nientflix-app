4d717c3ad0ae5a36904feea257d1d85a3699266d
Francesco Foglia <francescofogliajj@gmail.com>
2024-05-29 16:11:53 +0200

    new file:   src/components/Title.vue

---
+++ src/components/Title.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Nientflix</h1>
+</template>
+
+<style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+
+  h1 {
+    font-family: 'Bebas Neue', sans-serif;
+    font-size: 3rem;
+    color: #E50914;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.6);
+    letter-spacing: 0.1em;
+    margin: 0;
+  }
+</style>

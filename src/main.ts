3dc0b2de4a2e241c168b927fcd0c96a9eb59cfdf
Francesco Foglia <francescofogliajj@gmail.com>
2024-05-29 16:11:51 +0200

    modified:   src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import VueLazyLoad from 'vue-lazyload';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(VueLazyLoad, {
+  preLoad: 1.3,
+  error: 'https://placehold.co/300x450/000000/000000/webp',
+  loading: 'https://placehold.co/300x450/000000/000000/webp',
+  attempt: 1
+});
+
+app.mount('#app');

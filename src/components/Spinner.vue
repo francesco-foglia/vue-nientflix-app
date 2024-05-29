7e62d15a0a94ef4a4dddcd45282a38b2c714058d
Francesco Foglia <francescofogliajj@gmail.com>
2024-05-29 16:11:52 +0200

    new file:   src/components/Spinner.vue

---
+++ src/components/Spinner.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="fixed top-0 left-0 z-[1] w-full h-screen bg-[#242424] flex justify-center items-center">
+    <span class="loader"></span>
+  </div>
+</template>
+
+<style scoped>
+  .loader {
+    width: 48px;
+    height: 48px;
+    border: 5px solid #FFF;
+    border-bottom-color: transparent;
+    border-radius: 50%;
+    display: inline-block;
+    box-sizing: border-box;
+    animation: rotation 1s linear infinite;
+  }
+
+  @keyframes rotation {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+</style>
